26dd185b6a572890a6662e0c91ee6a3723c5d23d
Abhisek Chandra <chandra.abhisek@gmail.com>
2013-05-14 23:59:03 -0400

    first scala script

---
+++ ScalaProject/semicolon-example-script.scala
@@ -0,0 +1,6 @@
+// Trailing equals sign indicates more code on next line
+def equalsign = {
+  val reallySuperLongValueNameThatGoesOnForeverSoYouNeedANewLine ="wow that was a long value name";
+
+  println(reallySuperLongValueNameThatGoesOnForeverSoYouNeedANewLine);
+}
